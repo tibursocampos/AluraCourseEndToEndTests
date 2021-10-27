ca92ab3b57d7ad4bbd158a0002fe7dfbe338cff4
Raphael Campos <raphael.campos@streamer.com.br>
2021-10-26 21:55:05 -0300

    Aula 2 parte 1 finalizada.

---
+++ Alura.LeilaoOnline.Selenium/Testes/AoNavegarParaHome.cs
@@ -0,0 +1,41 @@
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoNavegarParaHome
+    {
+        private readonly IWebDriver driver;
+
+        //Setup
+        public AoNavegarParaHome(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
+        {        
+            //act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarProximosLeiloesNaPagina()
+        {
+            //act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/TesteNovo.cs
@@ -0,0 +1,32 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+
+    [Collection("Chrome Driver")]
+    public class TesteNovo
+    {
+        private readonly IWebDriver driver;
+
+        //Setup
+        public TesteNovo(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
+        {
+            //act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+    }
+}
